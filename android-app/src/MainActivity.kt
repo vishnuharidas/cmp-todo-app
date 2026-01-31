ba807ad58a561cd9305388763a52bf5f58803dc2
Vishnu Haridas <hvishnu999@gmail.com>
2026-01-31 15:45:06 +0400

    Remove the default titlebar in Android

--- android-app/src/MainActivity.kt
+++ android-app/src/MainActivity.kt
@@ -2,12 +2,14 @@ package hello.world
 
 import Screen
 import android.os.Bundle
+import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContent {
             Screen()
         }
